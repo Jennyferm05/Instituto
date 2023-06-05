274ab3854f295cf346aa2b21914c832f9c45fec5
Junior6580 <jsmedinah5873@gmail.com>
2023-06-04 23:37:55 -0500

    usuario

--- resources/views/colegio/usuarios.blade.php
+++ resources/views/colegio/usuarios.blade.php
@@ -4,14 +4,44 @@
 @endphp
 
 @section('content')
-<main class="py-4"></main>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-
-            </div>
+    <main class="py-4"></main>
+    <div id="tables" class="card">
+        <div class="card-header">
+            <h3 class="card-title">Usuarios<span> && </span>Permisos</h3>
         </div>
+        <!-- /.card-header -->
+        <div id="tables" class="card-body">
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                        <th>Rol</th>
+                        <th>Permisos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td>All others</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>U</td>
+                    </tr>
+                </tbody>
+
+            </table>
+        </div>
+        <!-- /.card-body -->
     </div>
-</div>
+    <!-- /.card -->
+    </div>
+    <!-- /.col -->
+    </div>
+    <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+    </div>
 @endsection
