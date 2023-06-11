69a0914094e59ed3056acc1cbccfda5453963c43
Junior6580 <jsmedinah5873@gmail.com>
2023-06-11 17:51:23 -0500

    JSM6580 - seeders y factorys para crear grados y jornadas al ejecutar los seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
         $this->call(PersonaSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(GradoSeeder::class);
+        $this->call(JornadaSeeder::class);
 
     }
 }
